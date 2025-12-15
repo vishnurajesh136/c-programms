bc3e69cf89bb495e80a36b3022587c7c0b81971c
vishnurajesh136 <vishnurajesh136@gmail.com>
2025-12-15 15:14:35 +0530

    Add files via upload

---
+++ subtration_of_matrixs_arrays.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	int a[3][3];
+	int b[3][3];
+	int c[3][3];
+	for (i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for (i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	for (i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		c[i][j]=a[i][j]-b[i][j];
+		}
+	}
+	for (i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf(" %d",c[i][j]);
+		}printf ("\n");
+	}
+	return 0;
+}
