2e4cac6434aadf5d0e62941a9d5635285eb5c2d9
vishnurajesh136 <vishnurajesh136@gmail.com>
2025-12-15 15:11:48 +0530

    Add files via upload

---
+++ biggest of two numbers.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int a,b;
+printf("enter a and b values");
+scanf("%d%d",&a,&b);
+if (a>b)
+printf("enter a is big");
+else
+printf("enter b is big");
+return 0;
+
+}
