25a811306e32ff1d7d421c23d57fc0d0e307e0ac
vishnurajesh136 <vishnurajesh136@gmail.com>
2025-12-15 15:13:04 +0530

    Add files via upload

---
+++ trance_of_matrix.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+ {
+ 	int rows, cols;
+ printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols], transpose[cols][rows];
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+}
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    printf("\nTransposed Matrix:\n");
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
